18590443bd1e0e95ef61b677a10dbcffc6be2028
Sharmila Prabhu <sharmilag.91@gmail.com>
2022-12-07 00:19:50 +0530

    user have to input a range

---
+++ main.js
@@ -0,0 +1,30 @@
+
+var main = document.getElementById("container");
+
+var label1 = document.createElement("LABEL");
+label1.innerHTML = "Enter a number to display a multiplication table:";
+main.appendChild(label1);
+
+var input1=document.createElement("INPUT");
+input1.setAttribute("type","number");
+input1.setAttribute("min",1);
+main.appendChild(input1);
+
+var label2 = document.createElement("LABEL");
+label2.innerHTML = "Enter a number to describe the range of a table:";
+main.appendChild(label2);
+
+var input2=document.createElement("INPUT");
+input2.setAttribute("type","number");
+input2.setAttribute("min",10);
+main.appendChild(input2);
+
+input2.addEventListener("input",()=>{
+	for (var j=1;j<=input2.value;j++){
+	var para = document.createElement("P");
+	para.innerHTML = j +" * "+input1.value+" = "+ j*input1.value;
+	main.appendChild(para);
+	}
+});
+
+
